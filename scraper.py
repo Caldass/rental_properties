74556a750f371aa551bee5e3baa2d8b0f3584fe8
Arthur <arthurmlcaldas@outlook.com>
2021-04-11 16:00:51 -0300

    Scraper sucessfuly scrapes all pages of a specified url

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from bs4 import BeautifulSoup
-import requests
 from selenium import webdriver
 import time
+
+#beautiful soup and selenium objects
 
 driver = webdriver.Chrome(executable_path='C:/Program Files/selenium/chromedriver.exe')
 
@@ -11,10 +12,6 @@ url = 'https://www.vivareal.com.br/aluguel/pernambuco/recife/apartamento_residen
 driver.get(url)
 html = driver.page_source
 
-#headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
-
-#soup objects
-#response = requests.get(url, headers = headers)
 soup = BeautifulSoup(html, 'html.parser', from_encoding="utf-8")
 
            
@@ -28,32 +25,26 @@ def set_key(dictionary, key, value):
      else:
          dictionary[key] = [dictionary[key], value]
 
+
 #dataframe with scraped data
 df = {}
 
 
-href = ''
-
 #follow the pages scrapped
 page = 0
 
-#number of posts scrapped
+#number of posts scraped
 post_num = 0
 
 #scrape while posts scraped < total posts possible
 while post_num < int(soup.find('strong', attrs = {"class" : "results-summary__count js-total-records"}).text.replace('.','').strip()):
-    #creates new url with next page href
-    #main_url = 'https://www.vivareal.com.br/aluguel/pernambuco/recife/apartamento_residencial/'
-    #url = main_url + href 
     
-    #chages soup objects
-    #response = requests.get(url, headers = headers)
+    #updates url after clicking page    
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser', from_encoding="utf-8")
-        
-    
+
     #storing all properties in a list
-    page_posts = [i for i in soup.find('div', attrs = {"class" : "results-list js-results-list"}).find_all('div',attrs = {"class" : "js-card-selector"})]
+    page_posts = [i for i in soup.find('div', attrs = {"class" : "results-list js-results-list"}).find_all('div', attrs = {"data-type" : 'property'})]
     
     #getting variables for each post
     for post in page_posts:
@@ -67,8 +58,11 @@ while post_num < int(soup.find('strong', attrs = {"class" : "results-summary__co
         #post size in m2
         set_key(df, 'area', post.find('span', attrs = {"class" : "property-card__detail-value js-property-card-value property-card__detail-area js-property-card-detail-area"}).text.strip())
         
+        #post bathrooms
+        set_key(df, 'bathrooms', post.find('li', attrs = {"class" : "property-card__detail-item property-card__detail-bathroom js-property-detail-bathroom"}).text.split()[0])
+        
         #post bedrooms
-        set_key(df, 'bedrooms', post.find('li', attrs = {"class" : "property-card__detail-item property-card__detail-bathroom js-property-detail-bathroom"}).text.split()[0])
+        set_key(df, 'bedrooms', post.find('li', attrs = {"class" : "property-card__detail-item property-card__detail-room js-property-detail-rooms"}).text.split()[0])
         
         #post parking spots
         set_key(df, 'parking_spots', post.find('li', attrs = {"class" : "property-card__detail-item property-card__detail-garage js-property-detail-garages"}).text.split()[0])
@@ -89,23 +83,15 @@ while post_num < int(soup.find('strong', attrs = {"class" : "results-summary__co
             set_key(df, 'fee', None)
         
         post_num += 1
-        
-        
-        print(post_num)
-
+                                            
     page += 1
-    #href = '#pagina=' + str(page + (1))
     print("page", page, "scrapped!")
     element = driver.find_element_by_partial_link_text('Próxima')
     driver.execute_script("arguments[0].click();", element)
     time.sleep(5)
-    
-    
 
-
+    
+#making dict into dataframe
 df = pd.DataFrame.from_dict(df, orient = 'index').transpose()
 df.to_excel('test_scrape.xlsx')
 
-
-
-#soup.find('div', attrs = {"class" : "results-list js-results-list"}).find_all('div',attrs = {"class" : "js-card-selector"})
