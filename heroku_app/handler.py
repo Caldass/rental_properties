1681f76aead50caaa28e3e35e6a46d6ae905e3f2
Arthur <arthurmlcaldas@outlook.com>
2021-04-25 05:11:16 -0300

    test on port again

--- heroku_app/handler.py
+++ heroku_app/handler.py
@@ -34,6 +34,6 @@ def predict():
 
 if __name__ == '__main__':
     #start flask
-    port = os.environ.get('PORT', 3000)
+    port = os.environ.get('PORT', 25858)
     app.run(host = 'localhost', port = port)
 
