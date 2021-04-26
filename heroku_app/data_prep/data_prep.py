a42c6f7b7fc70a683580bce18bcf6551da3d0748
Arthur <arthurmlcaldas@outlook.com>
2021-04-25 21:21:18 -0300

    changed data_prep encode

--- heroku_app/data_prep/data_prep.py
+++ heroku_app/data_prep/data_prep.py
@@ -11,7 +11,7 @@ class DataPrep(object):
         #get coord list
         with open("./parameters/coord_list.txt", "r") as file:
             self.coord_list = eval(file.readline())
-
+        #test
         with open("./parameters/x_cols.txt", "r", encoding= 'utf-8') as file:
             self.x_cols = eval(file.readline())
 
